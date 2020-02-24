3fb1e14bd1a758d2a0c67c0f9968765d213f9913
ngovanquan <quannv@sendo.vn>
2020-02-24 10:06:16 +0700

    first commit

---
+++ lib/main_prod.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_env_codemagic/environment.dart';
+import 'package:flutter_env_codemagic/main_common.dart';
+
+Future<void> main() async {
+  await mainCommon(Environment.prod);
+}
